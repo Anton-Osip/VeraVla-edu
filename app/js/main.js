61b96f24bdd7bf853f7c5cbf60fcf9582439f2be
Anton-Osip <63441692+Anton-Osip@users.noreply.github.com>
2022-05-17 22:28:39 +0300

    footer with media

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 import Swiper, { Navigation } from "swiper";
 
-var swiper = new Swiper(".ads__swiper", {
+var ads = new Swiper(".ads__swiper", {
   modules: [Navigation],
   slidesPerView: 3,
   spaceBetween: 20,
@@ -11,7 +11,23 @@ var swiper = new Swiper(".ads__swiper", {
   },
   breakpoints: {
     824: { slidesPerView: 3 },
-    594: { slidesPerView: 2 },
+    694: { slidesPerView: 2 },
+    320: { slidesPerView: 1 },
+  },
+});
+
+var jobs = new Swiper(".jobs__swiper", {
+  modules: [Navigation],
+  slidesPerView: 3,
+  spaceBetween: 20,
+  loop: true,
+  navigation: {
+    nextEl: ".jobs__button-next",
+    prevEl: ".jobs__button-prev",
+  },
+  breakpoints: {
+    824: { slidesPerView: 3 },
+    694: { slidesPerView: 2 },
     320: { slidesPerView: 1 },
   },
 });
